0048def10f2b4be44704db6f99387285f284411a
sherif98 <sheco_12345@hotmail.com>
2016-07-29 21:33:30 +0200

    Initial commit

---
+++ app/src/main/java/nanodegree/udacity/com/sunshine/MainActivity.java
@@ -0,0 +1,22 @@
+package nanodegree.udacity.com.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.main_fragment_container);
+        if (fragment == null) {
+            fragment = MainFragment.newInstance();
+            fragmentManager.beginTransaction().add(R.id.main_fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
